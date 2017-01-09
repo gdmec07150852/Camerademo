e42bbad409a407dfc449ff3871d3e436a93335e8
gdmec07150852 <840864093@qq.com>
2017-01-09 17:03:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/as/camerademo/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.as.camerademo;
+
+
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+    private ImageView imageview;
+    private File file;
+    private Camera camera;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(cn.edu.gdmec.s07150805.camerademo.R.layout.activity_main);
+        SurfaceView mSurfaceView= (SurfaceView) this.findViewById(cn.edu.gdmec.s07150805.camerademo.R.id.surfaceView1);
+        SurfaceHolder mSurfaceHolder=mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+    public void takePhoto(View v){
+        camera.takePicture(null,null,pictureCallback);
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera=Camera.open();
+        Camera.Parameters params=camera.getParameters();
+        params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        try{
+            camera.setPreviewDisplay(holder);
+        }catch(IOException exception){
+            camera.release();
+            camera=null;
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+    Camera.PictureCallback pictureCallback=new Camera.PictureCallback(){
+        @Override
+        public void onPictureTaken(byte[] data,Camera camera){
+            if (data!=null){
+                savePicture(data);
+            }
+        }
+    };
+    public void savePicture(byte[] data){
+        try{
+            String imageId=System.currentTimeMillis()+"";
+            String pathName=android.os.Environment.getExternalStorageDirectory().getPath()+"/";
+            File file=new File(pathName);
+            if (!file.exists()){
+                file.mkdirs();
+            }
+            pathName+=imageId+".jpeg";
+            file=new File(pathName);
+            if (!file.exists()){
+                file.createNewFile();
+            }
+            FileOutputStream fos=new FileOutputStream(file);
+            fos.write(data);
+            fos.close();
+            Toast.makeText(this,"已经保存在路径："+pathName,Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
